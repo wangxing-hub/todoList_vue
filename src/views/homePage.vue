89d921231694711363af06eb820282ca70451f7e
wangxing-hub <76116792+wangxing-hub@users.noreply.github.com>
2024-02-15 13:31:07 +0800

    Create homePage.vue

---
+++ src/views/homePage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <h1>hello</h1>
+    
+    <router-link to="/todoList">跳转到todoList</router-link><br>
+    
+    <!-- <el-button type="primary">Primary</el-button><br> -->
+    <img alt="Vue logo" src="../assets/logo.png">
+  </div>
+</template>
+
+<script>
+// import HelloWorld from '../components/HelloWorld.vue'
+export default {
+  name: 'homePage',
+  components: {
+  }
+}
+</script>
+
+<style>
+.home {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
